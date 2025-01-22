f0d6fe6d574a8bca7f4b58acb2ff0ed18e9b1afa
Hugues <zawadidigital@gmail.com>
2025-01-22 10:31:21 +0100

    rest of the files in the directory

---
+++ data_types.py
@@ -0,0 +1,22 @@
+first = "Dave"
+last = "Gray"
+
+# Concatenation
+fullname = first + " " + last
+print(fullname)
+
+fullname += "!"
+print(fullname)
+
+# Casting a number to a string
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "I like rock music from the " + decade + "s."
+print(statement)
+
+# Multiple lines
+multiline = '''
+Hey,
+'''
